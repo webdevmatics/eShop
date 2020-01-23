b0f14ec9cd2fc0b54ec53e853fc149fb0f8c3bec
subhendu <bhattasubhendu@gmail.com>
2020-01-23 08:02:29 +0545

    space fix

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -21,14 +21,16 @@
 
         <div class="form-group">
             <label for="">Description</label>
-            <textarea class="form-control" name="description" id="" rows="3">
-                {{$product->description}}
+            <textarea
+             class="form-control" name="description" id=""
+              rows="3">{{$product->description}}
             </textarea>
         </div>
 
         <div class="form-group">
             <label for="">Product cover image</label>
-            <input type="file" class="form-control-file" name="cover_img" id="" placeholder="" aria-describedby="fileHelpId">
+            <input type="file" class="form-control-file" name="cover_img" id="" placeholder=""
+                aria-describedby="fileHelpId">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
